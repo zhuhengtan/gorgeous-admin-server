a6b600d9541e449536714d2207f47f4bb783c171
zxalfred <zxsprout@foxmail.com>
2021-04-12 13:52:39 +0800

    feat: mysql connection&user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,19 +1,45 @@
 import { Context } from 'koa'
+import { getManager } from 'typeorm'
+
+import { User } from '../entity/user'
 
 export default class UserController {
-  static async listUsers(ctx: Context) {
-    ctx.body = 'ListUsers controller'
+  public static async listUsers(ctx: Context) {
+    const userRepository = getManager().getRepository(User)
+    const users = await userRepository.find()
+
+    ctx.status = 200
+    ctx.body = users
   }
 
-  static async showUserDetail(ctx: Context) {
-    ctx.body = `ShowUserDetail controller with ID = ${ctx.params.id}`
+  public static async showUserDetail(ctx: Context) {
+    const userRepository = getManager().getRepository(User)
+    const user = await userRepository.findOne(+ctx.params.id)
+
+    if (user) {
+      ctx.status = 200
+      ctx.body = user
+    } else {
+      ctx.status = 404
+    }
   }
 
-  static async updateUser(ctx: Context) {
-    ctx.body = `UpdateUser Controller with ID = ${ctx.params.id}`
+  public static async updateUser(ctx: Context) {
+    const userRepository = getManager().getRepository(User)
+    await userRepository.update(+ctx.params.id, ctx.request.body)
+    const updatedUser = await userRepository.findOne(+ctx.params.id)
+
+    if (updatedUser) {
+      ctx.status = 200
+      ctx.body = updatedUser
+    } else {
+      ctx.status = 404
+    }
   }
 
-  static async deleteUser(ctx: Context) {
-    ctx.body = `DeleteUser controller with ID = ${ctx.params.id}`
+  public static async deleteUser(ctx: Context) {
+    const userRepository = getManager().getRepository(User)
+    await userRepository.delete(+ctx.params.id)
+    ctx.status = 204
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,28 @@
 import Koa from 'koa'
 import cors from '@koa/cors'
 import bodyParser from 'koa-bodyparser'
+import { createConnection } from 'typeorm'
+import 'reflect-metadata'
 
 import router from './routes'
 import { logger } from './logger'
 
-// init koa app instance
-const app = new Koa()
+createConnection().then(() => {
+  // init koa app instance
+  const app = new Koa()
 
-// register middleware
-app.use(logger())
-app.use(cors())
-app.use(bodyParser())
+  // register middleware
+  app.use(logger())
+  app.use(cors())
+  app.use(bodyParser())
 
-// response to client request
-app.use(router.routes())
-  .use(router.allowedMethods())
+  // response to client request
+  app.use(router.routes())
+    .use(router.allowedMethods())
 
-// run server
-app.listen(3003)
+  // run server
+  app.listen(3003)
+}).catch((err: string) => {
+  console.log('TypeORM connection error:', err)
+})
+

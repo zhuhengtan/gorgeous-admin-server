bfc40b58ef205ba4db511eb63631ad9dc2f73956
hunterzhu <zhuhengtan@126.com>
2022-02-13 09:38:24 +0800

    chore: change port and database name

--- env/development.ts
+++ env/development.ts
@@ -4,14 +4,14 @@
 export const config = {
   env: 'development', //环境名称
   agreement: "http",
-  port: "3000",
+  port: "4000",
   database: {
     type: "mysql",
     host: "127.0.0.1",
     port: 3306,
     username: "root",
     password: "Codewave20190111",
-    database: "coop",
+    database: "gorgeous_admin_server",
     synchronize: true,
     entities: [
       "src/entity/*.ts"

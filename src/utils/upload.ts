6f1910610c4613462998f1edc126112b96762d22
zhuhengtan <zhuhengtan@126.com>
2023-12-06 14:39:52 +0800

    fix: upload problem

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,20 +1,20 @@
-import multer from 'koa-multer'
-import { randomUUID } from "crypto";
+import multer from '@koa/multer'
+import { randomUUID } from 'crypto'
 import envConfig from '../../env'
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-      cb(null, envConfig.staticPath)
-  },
-  filename: function (req, file, cb) {
-      let type = file.originalname.split('.')[1]
-      cb(null, `${randomUUID()}.${type}`)
-  }
+  // destination: function (req, file, cb) {
+  //   cb(null, envConfig.staticPath)
+  // },
+  // filename: function (req, file, cb) {
+  //   let type = file.originalname.split('.')[1]
+  //   cb(null, `${randomUUID()}.${type}`)
+  // },
 })
 //文件上传限制
 const limits = {
-    fields: 10,//非文件字段的数量
-    fileSize: 2 * 1024 * 1024,//文件大小 单位 b
-    files: 1//文件数量
+  fields: 10, //非文件字段的数量
+  fileSize: 10 * 1024 * 1024, //文件大小 单位 b
+  files: 1, //文件数量
 }
-export const upload = multer({limits, storage})
+export const upload = multer({ limits, storage })

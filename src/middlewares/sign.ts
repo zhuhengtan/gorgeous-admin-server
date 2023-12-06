b9ae10aaeb8b020ab1bdb3e6fd0ff66fd30bf6d6
zhuhengtan <zhuhengtan@126.com>
2023-12-06 15:03:44 +0800

    fix: sign bug

--- src/middlewares/sign.ts
+++ src/middlewares/sign.ts
@@ -1,8 +1,10 @@
 import md5 from 'md5'
 import envConfig from '../../env'
+import { ResCode } from '../utils/code';
+const unless = require('koa-unless')
 
 export default function () {
-  return async function (ctx: any, next:any) {
+  const mid = async function (ctx: any, next:any) {
     // 取出参数
     let params: any;
     if (ctx.method === 'GET') {
@@ -13,18 +15,25 @@ export default function () {
 
     // 判断签名
     if (!params.sign || !params.timestamp) {
-      ctx.fail('未传入签名或者时间戳！', 401)
-      return await next()
+      ctx.fail('未传入签名或者时间戳！', ResCode.Unauthorized)
+      return
     } else {
       const { timestamp, sign } = params;
+      if(Math.abs(timestamp - new Date().valueOf()) > 10000) {
+        ctx.fail('签名过期！', ResCode.Unauthorized)
+        return
+      }
       const keysArray = Object.keys(params).sort();
-      const checkSign = md5(`${keysArray.filter((key) => (key !== 'timestamp' && key !== 'sign')).map((key) => JSON.stringify(params[key])).join('')}${timestamp}${envConfig.signKey}`).toUpperCase()
+      const checkSign = md5(`${timestamp}${envConfig.signKey}`).toUpperCase()
       if(checkSign === sign) {
         return await next()
-      }else {
-        ctx.fail('签名错误！', 401)
+      } else {
+        ctx.fail('签名错误！', ResCode.Unauthorized)
         return
       }
     }
   }
+  mid.unless = unless
+
+  return mid
 }

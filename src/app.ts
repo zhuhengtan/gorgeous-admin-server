859e426b78dce56371367ed7c856a2754cb8f82e
zhuhengtan <zhuhengtan@126.com>
2023-12-06 14:57:53 +0800

    fix: app bug

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,21 @@ import sign from './middlewares/sign'
 import auth from './middlewares/auth'
 
 import { toBRouter } from './routes/toBRoutes'
+import { toCRouter } from './routes/toCRoutes'
 import { logger } from './logger'
-import { JWT_SECRET } from './utils/constants'
 
 import envConfig from '../env/index'
 import requestAdmin from './middlewares/requestAdmin'
+import requestUser from './middlewares/requestUser'
+import moment from 'moment'
+import timezone from 'moment-timezone'
+
+import { RateLimit, Stores } from 'koa2-ratelimit'
+
+moment.updateLocale('en', {
+  week: { dow: 1 }, // 1 表示周一
+})
+timezone.tz.setDefault('Asia/Shanghai')
 
 export class App {
   private app: Koa;
@@ -30,8 +40,21 @@ export class App {
   private async init() {
     // 开启数据库链接
     createConnection(envConfig.database as any).then(() => {
+      this.app.use(RateLimit.middleware({
+        interval: { min: 1 },  // 限制的时间窗口
+        max: 100,  // duration时间内的最大请求量
+        message: "请求过于频繁，请稍后再试。",  // 超出限制时的返回消息
+        store: new Stores.Redis({// 使用redis存储
+          host: envConfig.redis.host,
+          port: envConfig.redis.port,
+          db: envConfig.redis.db,
+        }),
+      }))
       this.app.use(logger())
-      this.app.use(cors())
+      if(envConfig.env === 'development') {
+        this.app.use(cors())
+      }
+      this.app
         .use(bodyParser())
         .use(formatResponse())
 
@@ -46,29 +69,45 @@ export class App {
       })
 
       if(envConfig.env === 'production') {
-        this.app.use(sign())
+        this.app.use(sign().unless({
+          path: [
+            '/api/b/common/upload',
+            '/api/c/common/upload'
+          ]
+        }))
       }
       
       // 自定义jwt验证失败返回信息
       this.app.use(function(ctx, next){
         return next().catch((err) => {
+          console.log(err)
           if (401 == err.status) {
             err.status = 200
             ctx.fail('登录失效或未登录！', 401)
+          } else if(err.message === 'jwt must be provided') {
+            ctx.fail('接口header必须传入Authorization字段！', 401)
           } else {
             throw err;
           }
         });
       })
       // jwt中间件
-      this.app.use(jwt({ secret: JWT_SECRET }).unless({
+      this.app.use(jwt({ secret: envConfig.jwtSecret }).unless({
         // 登录接口不需要验证
-        path: ['/api/b/auth/login']
+        path: [
+          '/api/b/auth/login',
+          '/api/c/user/get-login-sms-code',
+          '/api/c/user/login-with-sms-code',
+          /^\/api\/c\/public/,
+        ]
       }))
 
       // 将请求者用户信息放在ctx上下文中
       this.app.use(requestAdmin().unless({
         path: [/^\/api\/c/,]
+      }))
+      this.app.use(requestUser().unless({
+        path: [/^\/api\/b/,]
       }))
 
       // 鉴权
@@ -76,12 +115,17 @@ export class App {
         path: [
           '/api/b/auth/login',
           '/api/b/auth/admin-auth',
+          '/api/b/common/upload',
+          /^\/api\/c/,
         ]
       }))
 
       // 导入路由
       this.app.use(toBRouter.routes())
       this.app.use(toBRouter.allowedMethods())
+      
+      this.app.use(toCRouter.routes())
+      this.app.use(toCRouter.allowedMethods())
     }).catch((err: string) => {
       console.log('数据库链接错误：', err)
     })

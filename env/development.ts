c1e18c22b33157d7d76538404ff34137f96c2a53
zhuhengtan <zhuhengtan@126.com>
2023-12-19 15:37:10 +0800

    fix: database connect config

--- env/development.ts
+++ env/development.ts
@@ -16,6 +16,7 @@ export const config = {
     type: "mysql",
     host: "127.0.0.1",
     port: 3306,
+    charset: "utf8mb4",
     username: "root",
     password: "",
     database: "gorgeous_admin_server",
